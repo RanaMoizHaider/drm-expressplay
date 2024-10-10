745c73241968fbaf494a0d3123af4a546e4a6af2
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-10 13:54:52 +0500

    tried streaming using eme

--- resources/views/video/play.blade.php
+++ resources/views/video/play.blade.php
@@ -4,13 +4,13 @@
 
     <x-slot:styles>
         <!-- Video.js CSS -->
-        <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
+        <link href="https://vjs.zencdn.net/8.16.1/video-js.css" rel="stylesheet" />
     </x-slot:styles>
 
     <div class="flex justify-center items-center min-h-screen">
         <div class="relative w-full md:w-2/3 lg:w-1/2">
             <!-- Video.js player -->
-            <video id="video-player" class="video-js vjs-default-skin vjs-16-9" controls preload="auto" width="640" height="360" poster="{{ asset('img/placeholder.svg') }}">
+            <video id="video-player" class="video-js vjs-default-skin vjs-16-9" controls preload="auto" width="640" height="360">
                 <!-- Placeholder for poster image -->
                 <p class="vjs-no-js">
                     To view this video please enable JavaScript, and consider upgrading to a
@@ -23,33 +23,31 @@
 
     <x-slot:scripts>
         <!-- Video.js and DASH.js -->
-        <script src="https://unpkg.com/video.js/dist/video.js"></script>
+        <script src="https://vjs.zencdn.net/8.16.1/video.min.js"></script>
+        <!-- Include Dash.js library -->
+        <script src="https://cdn.dashjs.org/latest/dash.all.min.js"></script>
+        <!-- Include videojs-contrib-dash -->
         <script src="https://unpkg.com/videojs-contrib-dash/dist/videojs-dash.js"></script>
+        <!-- Include videojs-contrib-eme -->
+        <script src="https://unpkg.com/videojs-contrib-eme/dist/videojs-contrib-eme.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', function () {
-                var player = videojs('video-player', {
-                    techOrder: ['dash', 'html5'],
-                    html5: {
-                        dash: {}
-                    }
-                });
+                var player = videojs('video-player');
+                player.eme();
 
                 // Pass the PHP DRM configuration to JavaScript
                 var drmConfig = @json($drmConfig);
 
-                player.ready(function () {
-                    var src = {
                         src: '{{ $videoUrl }}',
-                        type: 'application/dash+xml'  // DASH manifest type
-                    };
+                var src = {
+                    type: 'application/dash+xml'  // DASH manifest type
+                };
 
                     var keySystems = {};
 
                     // Conditionally add Widevine DRM if the license URI is available
                     if (drmConfig.widevineLicenseUri) {
-                        keySystems['com.widevine.alpha'] = {
-                            licenseUri: drmConfig.widevineLicenseUri
-                        };
+                        keySystems['com.widevine.alpha'] = drmConfig.widevineLicenseUri;
                     }
 
                     // Conditionally add Marlin DRM if the license URI is available
@@ -78,6 +76,8 @@
                         src.keySystems = keySystems;
                     }
 
+                player.ready(function () {
+                    console.log(src);
                     player.src(src);
                 });
             });
